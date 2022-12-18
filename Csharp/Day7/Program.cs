d32ed5e70cd1edc9d64c6216da57cd9c1f7316d4
dj-viking <anders.swedishviking@gmail.com>
2022-12-18 17:50:42 -0500

    i don't understand....

--- Csharp/Day7/Program.cs
+++ Csharp/Day7/Program.cs
@@ -6,6 +6,7 @@ namespace Day7
         public string input = "";
         public string[] _lines = new string[] { "" };
         public LinkedList<string> _linkedList = new();
+        public string _currentPath = "";
         public class MyFile
         {
             public double _size = 0;
@@ -19,19 +20,23 @@ namespace Day7
         public class Output
         {
             public List<string> _children = new();
-            public string _dirname = "";
+            public string _dirPath = "";
             public List<MyFile> _files = new();
 
-            public Output(string dirname) {
-                this._dirname = dirname;
+            public Output(string dirPath)
+            {
+                this._dirPath = dirPath;
             }
         }
-        public List<Output> _terminalTree = new();
-        public class Dir {
-            public string _dirname = "";
+        public Dictionary<string, Output> _terminalTree = new();
+        public record DIR(string _dirPath, double _size);
+        public class Dir
+        {
+            public string _dirPath = "";
             public double _size = 0;
-            public Dir(string dirname, double size) {
-                this._dirname = dirname;
+            public Dir(string dirPath, double size)
+            {
+                this._dirPath = dirPath;
                 this._size = size;
             }
         }
@@ -58,8 +63,9 @@ namespace Day7
         public void Init()
         {
             this.input = "";
+            this._currentPath = "";
             this._lines = new string[] { "" };
-            this._terminalTree = new List<Output>();
+            this._terminalTree = new Dictionary<string, Output>();
             this._dirSizes = new List<Dir>();
         }
         public void AllocateTree()
@@ -71,18 +77,55 @@ namespace Day7
 
             string currentDir = "";
             string rootDir = this._linkedList.ElementAt(0).Split(" ")[2];
+            // start off the current path with root dir /
+            this._currentPath = rootDir;
+            this._terminalTree[rootDir] = new Output(rootDir);
             Console.WriteLine("***** start reading file system");
             for (int i = 0; i < this._linkedList.Count(); i++)
             {
                 Console.WriteLine("---- line item in linked list {0}", this._linkedList.ElementAt(i));
 
-                if (this._linkedList.ElementAt(i).Contains("$ cd ..")) continue;
+                if (this._linkedList.ElementAt(i).Contains("$ cd .."))
+                {
+                    // un append the current dir from the current path string to simulate changing directories one level up the tree
+                    string tempPath = "";
+                    string[] splitCurrentPath = this._currentPath.Split("/", StringSplitOptions.RemoveEmptyEntries);
+
+                    if (splitCurrentPath.Length > 1)
+                    {
+                        for (int k = 0; k < splitCurrentPath.Length - 1; k++)
+                        {
+                            tempPath += $"/{splitCurrentPath[k]}";
+                        }
+                        this._currentPath = tempPath;
+                    }
+                    else if (splitCurrentPath.Length == 1)
+                    {
+                        this._currentPath = "/";
+                    }
+
+
+                    continue;
+                }
 
                 if (this._linkedList.ElementAt(i).Contains("$ cd"))
                 {
                     // multiple dirs can have the same names so we need to include them in the entire terminal tree
+                    // append the dir we changed into on the current path string
+
                     currentDir = this._linkedList.ElementAt(i).Split(" ")[2];
-                    this._terminalTree.Add(new Output(currentDir));
+                    if (currentDir != rootDir)
+                    {
+                        if (this._currentPath.Split("/", StringSplitOptions.RemoveEmptyEntries).Length == 0)
+                        {
+                            this._currentPath += $"{currentDir}";
+                        }
+                        else
+                        {
+                            this._currentPath += $"/{currentDir}";
+                        }
+                    }
+
                     continue;
                 }
 
@@ -92,25 +135,49 @@ namespace Day7
                     for (int j = (i + 1); j < this._linkedList.Count(); j++)
                     {
                         string tempDirname2 = "";
+                        string tempDirPath = this._currentPath;
+
                         if (this._linkedList.ElementAt(j).Contains("$")) break;
 
                         if (this._linkedList.ElementAt(j).Contains("dir"))
                         {
                             tempDirname2 = this._linkedList.ElementAt(j).Split(" ")[1];
-                            this._terminalTree.Add(new Output(tempDirname2));
-                            this._terminalTree.Find(x => x._dirname == currentDir)!._children.Add(tempDirname2);
-                            this._terminalTree.Find(x => x._dirname == rootDir)!._children.Add(tempDirname2);
+                            if (this._currentPath != rootDir)
+                            {
+                                tempDirPath += $"/{tempDirname2}";
+                            }
+                            else
+                            {
+                                tempDirPath += $"{tempDirname2}";
+                            }
+
+                            if (!this._terminalTree.ContainsKey(tempDirPath))
+                            {
+                            }
+
+                            this._terminalTree.TryAdd(tempDirPath, new Output(tempDirPath));
+
+                            if (!this._terminalTree[this._currentPath]._children.Any(x => x == tempDirPath))
+                            {
+                                if (this._currentPath != rootDir)
+                                {
+                                    this._terminalTree[this._currentPath]._children.Add(tempDirPath);
+                                }
+                                else
+                                {
+                                    this._terminalTree[rootDir]._children.Add(tempDirPath);
+                                }
+                            }
                         }
 
                         double fileSize = 0;
                         if (double.TryParse(this._linkedList.ElementAt(j).Split(" ")[0], out fileSize))
                         {
                             string fileName = this._linkedList.ElementAt(j).Split(" ")[1];
-                            MyFile file = new MyFile(fileName, fileSize);
 
-                            this._terminalTree.Find(x => x._dirname == currentDir)!._files.Add(file);
+                            this._terminalTree[this._currentPath]._files.Add(new MyFile(fileName, fileSize));
                             Console.WriteLine("YYY reading terminal tree");
-                            // this.DebugTerminalTree();
+                            this.DebugTerminalTree();
                             Console.WriteLine("YYY end reading terminal tree");
                         }
 
@@ -121,37 +188,37 @@ namespace Day7
         }
         public void DebugTerminalTree()
         {
-            foreach (Output output in this._terminalTree)
+            foreach (Output output in this._terminalTree.Values)
             {
-                Console.WriteLine(" --- current dir {0} ", output._dirname);
-                foreach (MyFile file in this._terminalTree.Find(x => x._dirname == output._dirname)!._files)
+                Console.WriteLine(" --- current dir path {0} ", output._dirPath);
+                foreach (MyFile file in this._terminalTree[output._dirPath]!._files)
                 {
-                    Console.WriteLine("-- has this file --\n filename: {0} - filesize: {1}\nCCCC dir {3} has children [{2}]", file._name, file._size, string.Join(", ", this._terminalTree.Find(x => x._dirname == output._dirname)!._children), output._dirname);
+                    Console.WriteLine("-- has this file --\n filename: {0} - filesize: {1}\nCCCC dir {3} has children [{2}]", file._name, file._size, string.Join(", ", this._terminalTree[output._dirPath]._children), output._dirPath);
                 }
             }
         }
         public void CalculateDirSizes()
         {
-            foreach (Output output in this._terminalTree)
+            foreach (Output output in this._terminalTree.Values)
             {
                 double sum = 0;
 
-                foreach (MyFile file in this._terminalTree.Find(x => x._dirname == output._dirname)!._files)
+                foreach (MyFile file in this._terminalTree[output._dirPath]._files)
                 {
                     sum += file._size;
                 }
 
-                if (this._terminalTree.Find(x => x._dirname == output._dirname)!._children.Count() > 0)
+                if (this._terminalTree[output._dirPath]._children.Count() > 0)
                 {
-                    foreach (string childDir in this._terminalTree.Find(x => x._dirname == output._dirname)!._children)
+                    foreach (string childDirPath in this._terminalTree[output._dirPath]._children)
                     {
-                        foreach (MyFile file2 in this._terminalTree.Find(x => x._dirname == childDir)!._files)
+                        foreach (MyFile file2 in this._terminalTree[childDirPath]._files)
                         {
                             sum += file2._size;
                         }
                     }
                 }
-                this._dirSizes.Add(new Dir(output._dirname, sum));
+                this._dirSizes.Add(new Dir(output._dirPath, sum));
             }
             // this.DebugDirSizes();
         }
@@ -159,7 +226,7 @@ namespace Day7
         {
             foreach (Dir dir in this._dirSizes)
             {
-                Console.WriteLine(" in dir {0} there is size {1}", dir._dirname, dir._size);
+                Console.WriteLine(" in dir {0} there is size {1}", dir._dirPath, dir._size);
             }
         }
         public double GetSumOfDirsLessThan100k()
