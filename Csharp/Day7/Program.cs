423fd789201e82dc137c2ec091e35736587400a1
dj-viking <anders.swedishviking@gmail.com>
2022-12-17 21:31:00 -0500

    trying this out

--- Csharp/Day7/Program.cs
+++ Csharp/Day7/Program.cs
@@ -16,12 +16,26 @@ namespace Day7
                 this._size = size;
             }
         }
-        public class Output : List<MyFile>
+        public class Output
         {
-            public HashSet<string> _children = new();
+            public List<string> _children = new();
+            public string _dirname = "";
+            public List<MyFile> _files = new();
+
+            public Output(string dirname) {
+                this._dirname = dirname;
+            }
         }
-        public Dictionary<string, Output> _terminalTree = new();
-        public Dictionary<string, double> _dirSizes = new();
+        public List<Output> _terminalTree = new();
+        public class Dir {
+            public string _dirname = "";
+            public double _size = 0;
+            public Dir(string dirname, double size) {
+                this._dirname = dirname;
+                this._size = size;
+            }
+        }
+        public List<Dir> _dirSizes = new();
         public static void Main(string[] args)
         {
             new MainClass().Run(args);
@@ -45,8 +59,8 @@ namespace Day7
         {
             this.input = "";
             this._lines = new string[] { "" };
-            this._terminalTree = new Dictionary<string, Output>();
-            this._dirSizes = new Dictionary<string, double>();
+            this._terminalTree = new List<Output>();
+            this._dirSizes = new List<Dir>();
         }
         public void AllocateTree()
         {
@@ -66,9 +80,9 @@ namespace Day7
 
                 if (this._linkedList.ElementAt(i).Contains("$ cd"))
                 {
-                    // if dir already exists then don't add it to the terminal tree
+                    // multiple dirs can have the same names so we need to include them in the entire terminal tree
                     currentDir = this._linkedList.ElementAt(i).Split(" ")[2];
-                    this._terminalTree.TryAdd(currentDir, new Output());
+                    this._terminalTree.Add(new Output(currentDir));
                     continue;
                 }
 
@@ -83,9 +97,9 @@ namespace Day7
                         if (this._linkedList.ElementAt(j).Contains("dir"))
                         {
                             tempDirname2 = this._linkedList.ElementAt(j).Split(" ")[1];
-                            this._terminalTree.TryAdd(tempDirname2, new Output());
-                            this._terminalTree[currentDir]._children.Add(tempDirname2);
-                            this._terminalTree[rootDir]._children.Add(tempDirname2);
+                            this._terminalTree.Add(new Output(tempDirname2));
+                            this._terminalTree.Find(x => x._dirname == currentDir)!._children.Add(tempDirname2);
+                            this._terminalTree.Find(x => x._dirname == rootDir)!._children.Add(tempDirname2);
                         }
 
                         double fileSize = 0;
@@ -94,9 +108,9 @@ namespace Day7
                             string fileName = this._linkedList.ElementAt(j).Split(" ")[1];
                             MyFile file = new MyFile(fileName, fileSize);
 
-                            this._terminalTree[currentDir].Add(file);
+                            this._terminalTree.Find(x => x._dirname == currentDir)!._files.Add(file);
                             Console.WriteLine("YYY reading terminal tree");
-                            this.DebugTerminalTree();
+                            // this.DebugTerminalTree();
                             Console.WriteLine("YYY end reading terminal tree");
                         }
 
@@ -107,61 +121,61 @@ namespace Day7
         }
         public void DebugTerminalTree()
         {
-            foreach (string dir in this._terminalTree.Keys)
+            foreach (Output output in this._terminalTree)
             {
-                Console.WriteLine(" --- current dir {0} ", dir);
-                foreach (MyFile file in this._terminalTree[dir])
+                Console.WriteLine(" --- current dir {0} ", output._dirname);
+                foreach (MyFile file in this._terminalTree.Find(x => x._dirname == output._dirname)!._files)
                 {
-                    Console.WriteLine("-- has this file --\n filename: {0} - filesize: {1}\nCCCC dir {3} has children [{2}]", file._name, file._size, string.Join(", ", this._terminalTree[dir]._children), dir);
+                    Console.WriteLine("-- has this file --\n filename: {0} - filesize: {1}\nCCCC dir {3} has children [{2}]", file._name, file._size, string.Join(", ", this._terminalTree.Find(x => x._dirname == output._dirname)!._children), output._dirname);
                 }
             }
         }
         public void CalculateDirSizes()
         {
-            foreach (string dir in this._terminalTree.Keys)
+            foreach (Output output in this._terminalTree)
             {
                 double sum = 0;
 
-                foreach (MyFile file in this._terminalTree[dir])
+                foreach (MyFile file in this._terminalTree.Find(x => x._dirname == output._dirname)!._files)
                 {
                     sum += file._size;
                 }
 
-                if (this._terminalTree[dir]._children.Count() > 0)
+                if (this._terminalTree.Find(x => x._dirname == output._dirname)!._children.Count() > 0)
                 {
-                    foreach (string childDir in this._terminalTree[dir]._children)
+                    foreach (string childDir in this._terminalTree.Find(x => x._dirname == output._dirname)!._children)
                     {
-                        foreach (MyFile file2 in this._terminalTree[childDir])
+                        foreach (MyFile file2 in this._terminalTree.Find(x => x._dirname == childDir)!._files)
                         {
                             sum += file2._size;
                         }
                     }
                 }
-                this._dirSizes.TryAdd(dir, sum);
+                this._dirSizes.Add(new Dir(output._dirname, sum));
             }
-            this.DebugDirSizes();
+            // this.DebugDirSizes();
         }
         public void DebugDirSizes()
         {
-            foreach (string dir in this._dirSizes.Keys)
+            foreach (Dir dir in this._dirSizes)
             {
-                Console.WriteLine(" in dir {0} there is size {1}", dir, this._dirSizes[dir]);
+                Console.WriteLine(" in dir {0} there is size {1}", dir._dirname, dir._size);
             }
         }
         public double GetSumOfDirsLessThan100k()
         {
             List<double> dirs = new();
 
-            foreach (double size in this._dirSizes.Values)
+            foreach (Dir dir in this._dirSizes)
             {
-                if (size <= 100000)
+                if (dir._size <= 100000)
                 {
-                    dirs.Add(size);
+                    dirs.Add(dir._size);
                 }
             }
-            Console.WriteLine("------ showing dirs less than 100000 [{0}]", string.Join(", ", dirs));
+            Console.WriteLine("------ showing dirs less than 100000 [{0}]", string.Join(", ", dirs.Distinct()));
 
-            return dirs.Sum();
+            return dirs.Distinct().Sum();
         }
         public void PartOne()
         {
