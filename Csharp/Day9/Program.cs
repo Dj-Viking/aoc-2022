d40f2b2f07327f01f4bfd32750c81280b3550dea
dj-viking <anders.swedishviking@gmail.com>
2023-01-01 11:16:13 -0500

    creating classes for rope and graph

--- Csharp/Day9/Program.cs
+++ Csharp/Day9/Program.cs
@@ -2,10 +2,46 @@ namespace Day9
 {
     class MainClass
     {
+        public class Coordinates
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+            public Coordinates(int _x, int _y)
+            {
+                this.x = _x;
+                this.y = _y;
+            }
+        }
         public string input = "";
         public string[] _lines = new string[] { "" };
         public List<Dictionary<string, int>> OpMapList = new();
-        public string[][] Graph = new string[][] { new string[] { "" } };
+        Graph graph = new Graph();
+        public class Graph
+        {
+            public string[][] grid { get; set; } = new string[][] { new string[] { "" } };
+            public Graph() { }
+            public void PlotVisited(Start start, Rope rope)
+            {
+                this.grid[start.x][start.y] = "s";
+                this.grid[rope.tail.Coords.x][rope.tail.Coords.y] = "T";
+                this.grid[rope.head.Coords.x][rope.head.Coords.y] = "H";
+            }
+            public void DebugGraph()
+            {
+                Console.WriteLine("----- debugging graph");
+                foreach (string[] line in this.grid)
+                {
+                    foreach (string str in line)
+                    {
+                        Console.Write($" {str} ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+        public int MAX_DIM = 0;
+        public record Start(int x, int y);
+        public Start start = new Start(0, 0);
         public class OpName
         {
             public static readonly string R = "R";
@@ -13,6 +49,36 @@ namespace Day9
             public static readonly string U = "U";
             public static readonly string D = "D";
         }
+        public class Rope
+        {
+            public Head head = new Head();
+            public Tail tail = new Tail();
+            public class Head
+            {
+
+                public Coordinates Coords = new Coordinates(0, 0);
+                public Head() { }
+                public void SetLocation(int x, int y)
+                {
+                    this.Coords.x = x;
+                    this.Coords.y = y;
+                }
+            }
+            public class Tail
+            {
+                public Coordinates Coords = new Coordinates(0, 0);
+                public bool[][] visited { get; set; } = new bool[][] { new bool[] { false } };
+                public Tail() { }
+                public void SetLocation(int x, int y)
+                {
+                    this.Coords.x = x;
+                    this.Coords.y = y;
+                    this.visited[x][y] = true;
+                }
+            }
+
+        }
+        public Rope rope = new Rope();
         public static void Main(string[] args)
         {
             new MainClass().Run(args);
@@ -32,6 +98,8 @@ namespace Day9
             this.input = "";
             this._lines = new string[] { "" };
             this.OpMapList = new List<Dictionary<string, int>>();
+            this.rope = new Rope();
+            this.start = new Start(0, 0);
         }
         public void ParseOps()
         {
@@ -48,7 +116,7 @@ namespace Day9
         }
         public void InitGraph()
         {
-            int max_dim = 0;
+
             List<int> dimensionList = new();
             foreach (Dictionary<string, int> opObj in this.OpMapList)
             {
@@ -58,33 +126,33 @@ namespace Day9
                 }
             }
 
-            max_dim = dimensionList.Max() + 1;
+            this.MAX_DIM = dimensionList.Max() + 1;
 
             List<List<string>> lists = new();
+            List<List<bool>> boolLists = new();
 
-            for (int i = 0; i < max_dim; i++)
+            for (int i = 0; i < this.MAX_DIM; i++)
             {
                 List<string> tempList = new();
-                for (int j = 0; j < max_dim; j++)
+                List<bool> tempList2 = new();
+                for (int j = 0; j < this.MAX_DIM; j++)
                 {
                     tempList.Add(".");
+                    tempList2.Add(false);
                 }
                 lists.Add(tempList);
+                boolLists.Add(tempList2);
             }
 
-            this.Graph = lists.Select(x => x.ToArray()).ToArray();
+            this.graph.grid = lists.Select(list => list.ToArray()).ToArray();
+            this.rope.tail.visited = boolLists.Select(list => list.ToArray()).ToArray();
         }
-        public void DebugGraph()
+
+        public void InitPositionsOnGraph()
         {
-            Console.WriteLine("----- debugging graph");
-            foreach (string[] line in this.Graph)
-            {
-                foreach (string str in line)
-                {
-                    Console.Write($" {str} ");
-                }
-                Console.WriteLine();
-            }
+            this.start = new Start(this.MAX_DIM - 1, 0);
+            this.rope.head.SetLocation(this.MAX_DIM - 1, 0);
+            this.rope.tail.SetLocation(this.MAX_DIM - 1, 0);
         }
 
         public void GetInput(string fileName)
@@ -96,7 +164,9 @@ namespace Day9
         {
             this.ParseOps();
             this.InitGraph();
-            this.DebugGraph();
+            this.InitPositionsOnGraph();
+            this.graph.PlotVisited(this.start, this.rope);
+            this.graph.DebugGraph();
             Console.WriteLine("Part 1: {0}", "answer goes here");
         }
         public void PartTwo()
