72d70a83359e4320c9571cc598701d9c7f0decb8
dj-viking <anders.swedishviking@gmail.com>
2023-07-08 17:59:09 -0400

    start day 12

---
+++ PowerShell/Day12/Day12.ps1
@@ -0,0 +1,26 @@
+param(
+    [Parameter(Mandatory=$true, HelpMessage="Please enter an input filename")]
+    [System.String]$InputFilename
+)
+
+[String]$answer1 = "answer goes here"
+[String]$answer2 = "answer goes here"
+[String]$myInput = ""
+
+. $PSScriptRoot\..\ReadInput.ps1
+. $PSScriptRoot\..\ParseLines.ps1
+
+$myInput = Read-Input $InputFilename $PSScriptRoot
+$lines = Get-InputLines $myInput
+
+Function PartOne {
+    Write-Host "[INFO]: solving part one..." -ForegroundColor Cyan
+    Write-Host "[INFO]: part one answer is $answer1" -ForegroundColor Green
+}
+Function PartTwo {
+    Write-Host "[INFO]: solving part two..." -ForegroundColor Cyan
+    Write-Host "[INFO]: part two answer is $answer2" -ForegroundColor Green
+}
+
+PartOne
+PartTwo
